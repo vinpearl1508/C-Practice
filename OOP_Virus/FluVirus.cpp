0f63557d5577cc8206f94fa63512058c1302b3ca
Vien Ho <kiyo.kiyo1508@gmail.com>
2019-01-07 14:52:39 +0700

    + Design FluVirus class

---
+++ OOP_Virus/FluVirus.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include "Virus.h"
+#include "FluVirus.h"
+using namespace std;
+
+fluVirus::fluVirus() {
+	doBorn();
+	initResistance();
+}
+fluVirus::~fluVirus() {
+	m_color = 0;
+}
+fluVirus::fluVirus(const fluVirus* fluVirus) {
+	//Copy all attributes of current virus
+	*this = *fluVirus;
+}
+
+void fluVirus::doBorn() {
+	loadADNInformation();
+	int n = 1 + rand() % 2;
+	if (n == 1) {
+		m_color = BLUE;
+	}
+	else {
+		m_color = RED;
+	}
+}
+vector<virus*> fluVirus::doClone() {
+	vector<virus*> virusList;
+	fluVirus *newFluVirus = new fluVirus(this);
+	virusList.push_back(newFluVirus);
+	return virusList;
+}
+void fluVirus::doDie() {
+	this->~fluVirus();
+	cout << "The virus was died";
+}
+void fluVirus::initResistance() {
+	if (m_color == RED) {
+		setM_resistance(10 + rand() % 11); // 10 - 20
+	}
+	else {
+		setM_resistance(10 + rand() % 6); // 10 - 15
+	}
+}
+
+void fluVirus::setM_color(int m_color) {
+	this->m_color = m_color;
+}
+int fluVirus::getM_color() {
+	return m_color;
+}
---
+++ OOP_Virus/FluVirus.h
@@ -0,0 +1,36 @@
+#ifndef FLU_VIRUS_H
+#define FLU_VIRUS_H
+#include "Virus.h"
+//Inherit Virus
+class fluVirus : public virus
+{
+public:
+	//Constructor
+	fluVirus();
+	//Destructor
+	~fluVirus();
+	//Copy Constructor
+	fluVirus(const fluVirus*);
+
+	//Call LoadADNInformation() and randomly initialize Flu m_color
+	void doBorn() override;
+	//A new virus that all its attributes are copied to a new one
+	std::vector<virus*> doClone() override;
+	//Release all virus data
+	void doDie() override;
+	//Set value for m_resistance
+	void initResistance() override;
+	
+	//Setter, getter methods
+	void setM_color(int);
+	int getM_color();
+private:
+	//the color of Flu virus is blue or red
+	int m_color;
+	enum Color
+	{
+		BLUE = 1,
+		RED = 2
+	};
+};
+#endif // !FLU_VIRUS_H
